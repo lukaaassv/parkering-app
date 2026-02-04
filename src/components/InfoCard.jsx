1381187054467f7bf599557cc885cef96334e83c
lukaaassv <lukassvens01@gmail.com>
2026-02-04 20:29:01 +0100

    Changed text

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -6,7 +6,7 @@ function InfoCard(){
             <h2>Information om parkering</h2>
         </div>
         <div className="info-body">
-            <p>Under vecka 42 - 18 gäller datumparkering i centrala rättvik för att underlätta för snöröjningen. Här kan du på ett enkelt sätt se vilka parkeringsregler som gäller för varje dag.</p>
+            <p>Under vecka 42 - 18 gäller datumparkering i centrala rättvik för att underlätta för snöröjningen. Här kan du på ett enkelt sätt se vilka parkeringsregler som gäller för dagen.</p>
         </div>
     </div>
     )
